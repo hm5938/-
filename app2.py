e49612f2a432c3df17f2737cc7cfcad97f58df33
JJIaa <110366681+JJIaa@users.noreply.github.com>
2022-08-02 16:38:25 +0900

    포스팅하기

--- app2.py
+++ app2.py
@@ -70,6 +70,11 @@ def restaurant_post():
 
     return jsonify({'msg': '등록 완료'})
 
+@app.route("/post", methods=["GET"])
+def restaurant_get():
+    post_list = list(db.restaurants.find({},{'_id': False}))
+    return jsonify({'posts':post_list})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
